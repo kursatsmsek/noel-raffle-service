cd12c018165800ac24c815206876aa0f280bb501
kursatsmsek <kursatsimsek@protonmail.ch>
2023-11-21 14:31:54 +0300

    localization messages added

--- src/main/java/com/kursatdev/noelraffleservice/NoelRaffleserviceApplication.java
+++ src/main/java/com/kursatdev/noelraffleservice/NoelRaffleserviceApplication.java
@@ -2,9 +2,29 @@ package com.kursatdev.noelraffleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class NoelRaffleserviceApplication {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource bundleMessageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
     public static void main(String[] args) {
         SpringApplication.run(NoelRaffleserviceApplication.class, args);
     }
--- src/main/java/com/kursatdev/noelraffleservice/controller/NoelRaffleController.java
+++ src/main/java/com/kursatdev/noelraffleservice/controller/NoelRaffleController.java
@@ -3,9 +3,12 @@ package com.kursatdev.noelraffleservice.controller;
 import com.kursatdev.noelraffleservice.dto.NoelRaffleData;
 import com.kursatdev.noelraffleservice.service.NoelRaffleService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping(path = "noel")
@@ -15,20 +18,25 @@ public class NoelRaffleController {
 
     private final NoelRaffleService noelRaffleService;
 
-    @PostMapping ("/raffle")
-    public ResponseEntity<?> noelRaffle(@RequestBody NoelRaffleData noelRaffleData) {
-        try {
-            if (noelRaffleData.getParticipants().size() > 500) {
-                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Participants count cannot exceed 500");
-            } else if (!noelRaffleData.getTitle().isBlank()) {
-                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Title cannot be empty");
-            }
+    private final MessageSource messageSource;
 
-            noelRaffleService.executeRaffle(noelRaffleData);
-            return ResponseEntity.ok().build();
+    @PostMapping ("/raffle")
+    public ResponseEntity<?> noelRaffle(@RequestBody NoelRaffleData noelRaffleData,
+                                        @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        if (noelRaffleData.getParticipants().size() > 500) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(messageSource.getMessage("participants-limit", null, locale));
+        } else if (noelRaffleData.getParticipants().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(messageSource.getMessage("participants-cannot-be-empty", null, locale));
+        } else if (noelRaffleData.getTitle() == null || noelRaffleData.getTitle().isBlank()) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(messageSource.getMessage("title-cannot-be-null", null, locale));
+        }
+
+        try {
+            //noelRaffleService.executeRaffle(noelRaffleData);
+            return ResponseEntity.ok().body(messageSource.getMessage("noel-raffle-success", null, locale));
 
         } catch (Exception exception) {
-            return ResponseEntity.internalServerError().body("Something went wrong");
+            return ResponseEntity.internalServerError().body(messageSource.getMessage("something-went-wrong", null, locale));
         }
     }
 
