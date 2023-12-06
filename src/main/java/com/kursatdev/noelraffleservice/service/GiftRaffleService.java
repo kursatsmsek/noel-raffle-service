c74f40d9ecb534e23062d64719ce277e4207b387
kursatsmsek <kursatsimsek@protonmail.ch>
2023-12-06 19:52:23 +0300

    gift raffle service completed with listener

--- src/main/java/com/kursatdev/noelraffleservice/service/GiftRaffleService.java
+++ src/main/java/com/kursatdev/noelraffleservice/service/GiftRaffleService.java
@@ -5,7 +5,12 @@ import com.kursatdev.noelraffleservice.model.Gift;
 import com.kursatdev.noelraffleservice.model.GiftRaffle;
 import com.kursatdev.noelraffleservice.model.Participant;
 import com.kursatdev.noelraffleservice.repository.GiftRaffleRepository;
-import lombok.AllArgsConstructor;
+import com.kursatdev.noelraffleservice.repository.GiftRepository;
+import com.kursatdev.noelraffleservice.repository.ParticipantRepository;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -14,19 +19,71 @@ import java.util.Locale;
 import java.util.Map;
 
 @Service
-@AllArgsConstructor
 public class GiftRaffleService {
 
     private final GiftRaffleRepository giftRaffleRepository;
 
     private final MailService mailService;
 
+    private final ParticipantService participantService;
+
+    private final GiftService giftService;
+
+    private final RaffleService raffleService;
+
+    private final ParticipantRepository participantRepository;
+
+    private final GiftRepository giftRepository;
+
+    private final DirectExchange directExchange;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public GiftRaffleService(GiftRaffleRepository giftRaffleRepository, MailService mailService, ParticipantService participantService, GiftService giftService, RaffleService raffleService, ParticipantRepository participantRepository, GiftRepository giftRepository, DirectExchange directExchange, RabbitTemplate rabbitTemplate) {
+        this.giftRaffleRepository = giftRaffleRepository;
+        this.mailService = mailService;
+        this.participantService = participantService;
+        this.giftService = giftService;
+        this.raffleService = raffleService;
+        this.participantRepository = participantRepository;
+        this.giftRepository = giftRepository;
+        this.directExchange = directExchange;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value("${sr.rabbit.routing.name}")
+    private String routingName;
+
     public void executeGiftRaffle(GiftRaffleData giftRaffleData, Locale locale) {
         GiftRaffle giftRaffle = convertDataToGiftRaffle(giftRaffleData);
-
         giftRaffleRepository.save(giftRaffle);
-        //Map<Participant, Gift> matches = giftRaffle.performRaffle();
-        //matches.forEach((Participant participant, Gift gift) -> mailService.sendGiftEmail(participant.getEmail(), participant.getDisplayName(), gift.getName(), giftRaffle.getTitle(), locale));
+
+        List<Participant> participants = giftRaffleData.getParticipants();
+        for (Participant participant : participants) {
+            participant.setGiftRaffle(giftRaffle);
+            participant.setLocale(locale);
+            participantRepository.save(participant);
+        }
+
+        List<Gift> gifts = giftRaffleData.getGifts();
+        for (Gift gift: gifts) {
+            gift.setGiftRaffle(giftRaffle);
+            giftRepository.save(gift);
+        }
+
+
+        Map<Long, Long> matches = raffleService.performGiftRaffle(participants, gifts);
+        rabbitTemplate.convertAndSend(directExchange.getName(), routingName, matches);
+    }
+
+    @RabbitListener(queues = "${sr.rabbit.queue.name}")
+    public void sendEmails(Map<Long, Long> matches) {
+        matches.forEach((personId, giftId) -> {
+            Participant person = participantService.getParticipantsById(personId);
+            Gift gift = giftService.getGiftById(giftId);
+
+            mailService.sendGiftEmail(person.getEmail(), person.getDisplayName(), gift.getName(), person.getGiftRaffle().getTitle(), person.getLocale());
+        });
     }
 
     public GiftRaffle convertDataToGiftRaffle(GiftRaffleData giftRaffleData) {
@@ -35,18 +92,6 @@ public class GiftRaffleService {
         giftRaffle.setRaffleGroup(giftRaffleData.getGroup());
         giftRaffle.setSector(giftRaffleData.getSector());
         giftRaffle.setRaffleDate(new Date());
-
-        List<Participant> participants = giftRaffleData.getParticipants();
-        for (Participant participant : participants) {
-            participant.setGiftRaffle(giftRaffle);
-        }
-        giftRaffle.setParticipants(participants);
-
-        List<Gift> gifts = giftRaffleData.getGifts();
-        for (Gift gift: gifts) {
-            gift.setGiftRaffle(giftRaffle);
-        }
-        giftRaffle.setGifts(gifts);
 
         return giftRaffle;
     }
--- src/main/java/com/kursatdev/noelraffleservice/service/RaffleService.java
+++ src/main/java/com/kursatdev/noelraffleservice/service/RaffleService.java
@@ -1,12 +1,11 @@
 package com.kursatdev.noelraffleservice.service;
 
+import com.kursatdev.noelraffleservice.model.Gift;
 import com.kursatdev.noelraffleservice.model.Participant;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 @Service
 public class RaffleService {
@@ -23,5 +22,20 @@ public class RaffleService {
         }
 
         return matches;
+    }
+
+    public Map<Long, Long> performGiftRaffle(List<Participant> participants, List<Gift> gifts) {
+        Map<Long, Long> result = new HashMap<>();
+        List<Gift> availableGifts = new ArrayList<>();
+
+        gifts.forEach(gift -> IntStream.range(0, gift.getCount()).forEach(i -> availableGifts.add(gift)));
+
+        List<Participant> shuffledParticipants = new ArrayList<>(participants);
+        Collections.shuffle(shuffledParticipants);
+
+        IntStream.range(0, Math.min(shuffledParticipants.size(), availableGifts.size()))
+                .forEach(i -> result.put(shuffledParticipants.get(i).getId(), availableGifts.get(i).getId()));
+
+        return result;
     }
 }
